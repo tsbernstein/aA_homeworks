43c1a582683fdf4cc799b540685137f4ce275e3d
Tyler Bernstein <tsbernstein2593@gmail.com>
2021-05-13 00:42:00 -0500

    rspec practice homework

---
+++ W4D4/skeleton/spec/dessert_spec.rb
@@ -0,0 +1,65 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  let(:chef) { double("chef") }
+  let(:brownie) { Dessert.new("brownie", 60, "chef") }
+
+  describe "#initialize" do
+    it "sets a type" do
+      expect(brownie.type).to eq('brownie')
+    end
+
+    it "sets a quantity" do
+      expect(brownie.quantity).to eq(60)
+    end
+
+    it "starts ingredients as an empty array" do
+      expect(brownie.ingredients).to be_empty
+    end
+
+    it "raises an argument error when given a non-integer quantity" do
+      expect { brownie.temp("banana")}.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do
+      brownie.add_ingredient("banana")
+      expect(brownie.ingredients).to include("banana")
+    end
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array" do
+      ingredients = ["banana", "walnuts", "flour"]
+
+      ingredients.each do |ingredient|
+        brownie.add_ingredient(ingredient)
+      end
+
+      expect(brownie.ingredients).to eq(ingredients)
+      brownie.mix!
+      expect(brownie.ingredients).not_to eq(ingredients)
+      expect(brownie.ingredients.sort).to eq(ingredients.sort)
+    end
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity"
+
+    it "raises an error if the amount is greater than the quantity"
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name"
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in"
+  end
+end
